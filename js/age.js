c8d3683742dd7c8da562a97503e0f12b60913728
Md. Yamin Hossain <needyamin@gmail.com>
2022-05-31 07:19:35 +0600

    Update age.js

--- js/age.js
+++ js/age.js
@@ -1,12 +1,10 @@
 function calculate_age(dob) { 
     var diff_ms = Date.now() - dob.getTime();
     var age_dt = new Date(diff_ms); 
-    return Math.abs(age_dt.getUTCFullYear() - 1995);
-    }
-/* current date; */    
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = yyyy + ',' + mm + ',' + dd;    
-document.getElementById("needyamin_age").innerHTML = calculate_age(new Date(today)) /*today*/
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+//console.log(calculate_age(new Date(2022, 05, 31)));
+//console.log(calculate_age(new Date(1995, 11, 15)));
+document.getElementById("needyamin_age").innerHTML = calculate_age(new Date(1995, 11, 15));
